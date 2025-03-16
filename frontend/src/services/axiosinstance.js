7209e9d53fe2d006c039970075925164e658cf01
ishan-dg-coldmonk <ishan.dasgupta.2003@gmail.com>
2025-03-16 22:32:15 +0530

    backend update

--- frontend/src/services/axiosinstance.js
+++ frontend/src/services/axiosinstance.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const instance = axios.create({
     // baseURL: 'http://localhost:3001/',
-    baseURL: 'http://localhost:8001/',
+    baseURL: 'http://65.2.81.206:8001/',
 })
 
 instance.interceptors.request.use((request) => {
